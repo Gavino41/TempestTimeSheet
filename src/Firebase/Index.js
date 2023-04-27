93434c63c03d2d8335fa11244140f21e5adf49b0
gavino41 <gavinosborne6910@gmail.com>
2023-04-27 18:05:33 +0900

    login screen fully functional with design and auth

--- src/Firebase/Index.js
+++ src/Firebase/Index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import * as firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,4 +29,3 @@ const auth = firebase.auth();
 
 export { auth };
 
-const analytics = getAnalytics(app);
